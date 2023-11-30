8d0d2874c19a9fdd51ff09e1d8541c9ca827945e
armandoLopz <104444680+armandoLopz@users.noreply.github.com>
2023-11-30 07:57:59 -0400

    Add code to sum the matrix.

--- sum.py
+++ sum.py
@@ -1,1 +1,11 @@
+def sumMatrix(matrix1, matrix2):
 
+    if len(matrix1) == len(matrix2) and len(matrix1[0]) == len(matrix2[0]):
+        matrixResult = [] 
+        for i in range(len(matrix1)):
+            matrixResult.append([])
+            for j in range (len(matrix1[0])):
+                matrixResult[i].append(matrix1[i][j] + matrix2[i][j])      
+        return matrixResult
+    else:
+        return None
