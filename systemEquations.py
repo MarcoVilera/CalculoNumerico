049a9be6558781b64799ad5b2cea8d7ecd658f9b
armandoLopz <104444680+armandoLopz@users.noreply.github.com>
2023-12-04 12:10:13 -0400

    Se agrego mejoras al codigo.

--- systemEquations.py
+++ systemEquations.py
@@ -1,40 +1,35 @@
-import numpy as np
-import matplotlib.pyplot as plt
+import numpy as np 
+import matplotlib.pyplot as plt 
 
-""" 
-Sistema de ecuaciones
-    x + y + z = 6
-    2x + y - z = 1
-    3x - y + 2z = 7
-"""
+#systemEquations 
 
-def ecuacion1(x):
-    return (6-x)
+"""A = np.array([[-2,5,9],
+              [7,1,1],
+              [-3,7,-1]])
 
-def ecuacion2(x):
-    return ((1-2*x)/-1)
+#vectorSolution 
+B = np.array([1,6,-26])"""
 
-def ecuacion3(x):
-    return ((7-3*x)/2)
+A = np.array([[2,3,-1],
+              [-1,4,2],
+              [1,1,1]])
 
-x = np.linspace(-5, 5, 100)
+B = np.array([7,2,4])
 
-y1 = ecuacion1(x)
-y2 = ecuacion2(x)
-y3 = ecuacion3(x)
+solutionSystem = np.linalg.solve(A,B)
 
-fig, ax = plt.subplots()
-ax.plot(x, y1, label='x + y + z = 6')
-ax.plot(x, y2, label='2x + y - z = 1')
-ax.plot(x, y3, label='3x - y + 2z = 7')
-ax.legend()
+x_vals = np.linspace(-5,5,100)
+y_vals = np.linspace(-5,5,100)
 
-punto_interseccion = np.array([np.linalg.solve([[1, 1, 1], [2, 1, -1], [3, -1, 2]], [6, 1, 7])])
+plt.plot(x_vals, (7 + 2*x_vals + 3*y_vals), label='-2x + 5y + 9z = 1')
+plt.plot(x_vals, (2 - 1*x_vals + 4*y_vals), label='7x + y + z = 6')
+plt.plot(x_vals, (4 + 1*x_vals + y_vals), label='-3x + 7y - z = -26')
 
-print('Punto de intersección:', punto_interseccion)
+x_inter = solutionSystem[0]
+y_inter = solutionSystem[1]
 
+plt.scatter(x_inter, y_inter, c='red', marker='o', s=100)
 
-ax.scatter(punto_interseccion[0][0], punto_interseccion[0][1], c='r', marker='o', s=100)
-
-
+plt.legend()
+plt.grid()
 plt.show()
